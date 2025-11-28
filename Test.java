8843a9d56c7325164f718d8b3f8011681cfe4fad
prakashrio99 <prakashrio99@gmail.com>
2025-11-28 09:16:48 +0530

    goals

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins!");
+        System.out.println("Hello prakash you will be in good position one day!");
     }
 }
